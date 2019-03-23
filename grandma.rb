fa38dd6461ec7f48bcf8ec7ca771639403f3cb6f
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-03-23 14:49:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(greetGrandma)
   if greetGrandma == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif greetGrandma == greetGrandma.downcase
+  elsif greetGrandma == greetGrandma.capitalize
      "HUH?! SPEAK UP, SONNY!"
    end
 end
